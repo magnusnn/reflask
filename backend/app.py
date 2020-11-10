163aa7980dbf7ade5e80f3606994b8a8fc4698c3
Magnus Nermark <magnusnermark@gmail.com>
2020-11-10 20:28:22 +0100

    backend cleanup and npm run simplified by using flask dev server instead

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,25 @@
 import os
+import logging
 from flask import Flask, request, send_from_directory
-from livereload import Server
 from classifier import classifyImage
 from reverseProxy import proxyRequest
 
-app = Flask(__name__, static_folder="static")
-app.debug = True
+MODE = os.getenv('FLASK_ENV')
+DEV_SERVER_URL = 'http://localhost:3000'
 
-APP_PORT = 5000
-MODE = os.getenv("MODE")
-DEV_SERVER_URL = "http://localhost:3000"
+logging.getLogger('werkzeug').disabled = True
+app = Flask(__name__, static_folder='static')
 
-@app.route("/", defaults={"path": ""})
-@app.route("/<path:path>")
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
 def index(path):
     # Route through webpack-dev-server if development mode.
-    if MODE == "development":
+    if MODE == 'development':
         return proxyRequest(DEV_SERVER_URL, request.path)
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
+    if path and os.path.exists(app.static_folder + '/' + path):
        return send_from_directory(app.static_folder, path)
     else:
-        return send_from_directory(app.static_folder, "index.html")
+        return send_from_directory(app.static_folder, 'index.html')
 
 @app.route('/classify', methods=['GET', 'POST'])
 def classify():
@@ -30,16 +29,7 @@ def classify():
     
             result = classifyImage(file)
  
-            print("Model classification: " + result)
+            print('Model classification: ' + result)
             
             return result
     return None
-
-if __name__ == "__main__":
-    server = Server(app.wsgi_app)
-
-    # Avoid hot reload when image is received.
-    server.watch("/uploads/*", ignore=True)
-
-    # Serve app with live reload.
-    server.serve(port=APP_PORT)
