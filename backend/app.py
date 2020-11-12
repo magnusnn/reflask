1873f578f7c9b590e833109a6e3e6a2b5fc18fbf
Magnus Nermark <magnusnermark@gmail.com>
2020-11-12 13:24:15 +0100

    removed custom loading of static files and used render_template instead for real this time

--- backend/app.py
+++ backend/app.py
@@ -1,35 +1,30 @@
 import os
 import logging
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, render_template
 from classifier import classifyImage
 from reverseProxy import proxyRequest
 
 MODE = os.getenv('FLASK_ENV')
-DEV_SERVER_URL = 'http://localhost:3000'
+DEV_SERVER_URL = 'http://localhost:3000/'
 
 logging.getLogger('werkzeug').disabled = True
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, template_folder='')
 
-@app.route('/', defaults={'path': ''})
+@app.route('/')
 @app.route('/<path:path>')
-def index(path):
+def index(path=''):
     # Route through webpack-dev-server if development mode.
     if MODE == 'development':
-        return proxyRequest(DEV_SERVER_URL, request.path)
-    if path and os.path.exists(app.static_folder + '/' + path):
-       return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, 'index.html')
+        return proxyRequest(DEV_SERVER_URL, path)
+    return render_template('index.html')
 
-@app.route('/classify', methods=['GET', 'POST'])
+@app.route('/classify', methods=['POST'])
 def classify():
-    if request.method == 'POST':
-        if (request.files['image']): 
-            file = request.files['image']
-    
-            result = classifyImage(file)
- 
-            print('Model classification: ' + result)
-            
-            return result
-    return None
+    if (request.files['image']): 
+        file = request.files['image']
+
+        result = classifyImage(file)
+
+        print('Model classification: ' + result)
+        
+        return result
